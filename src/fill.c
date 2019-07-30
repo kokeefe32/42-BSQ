bfe0a71a539c3bf1c042a79dcf29f0d8579b027c
Joseph Iwok <jiwok@e1z2r9p28.42.us.org>
2019-07-29 17:36:08 -0700

    further tests and print output

--- src/fill.c
+++ src/fill.c
@@ -6,12 +6,34 @@
 /*   By: jiwok <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 12:12:10 by jiwok             #+#    #+#             */
-/*   Updated: 2019/07/29 14:46:41 by jiwok            ###   ########.fr       */
+/*   Updated: 2019/07/29 17:35:37 by jiwok            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "funcs.h"
+
+void	display_nice_map(char map[4][8], int nrows, int ncols, char ch, sqre a)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < nrows)
+	{
+		j = 0;
+		while (j < ncols)
+		{
+			if (i >= (a.i + 1 - a.size) && i <= a.i && j >= (a.j + 1 - a.size) && j <= a.j)
+				printf("%c ", ch);
+			else
+				printf("%c ", map[i][j]);
+			j += 1;
+		}
+		printf("\n");
+		i += 1;
+	}
+}
 
 void	print_size_map(int map[4][8], int nrows, int ncols)
 {
@@ -34,9 +56,9 @@ void	print_size_map(int map[4][8], int nrows, int ncols)
 
 int		find_smallest(int i, int j, int k)
 {
-	if (i < j && i < k)
+	if (i <= j && i <= k)
 		return (i);
-	else if (j < i && j < k)
+	else if (j <= i && j <= k)
 		return (j);
 	else
 		return (k);
@@ -84,6 +106,7 @@ sqre	size_map(char map[4][8], char *defs, int nrows, int ncols)
 	}
 	print_size_map(sizemap, nrows, ncols);
 	printf("size of bq square is %i. location is %i,%i\n", a.size, a.i, a.j);
+	display_nice_map(map, nrows, ncols, defs[2], a);
 	return (a);
 }
 
@@ -91,7 +114,7 @@ int		main(void)
 {
 	char map[4][8] = {
 		{'.', '.', '.', '.', '.', '.', 'o', '.'},
-		{'o', 'o', '.', '.', 'o', '.', '.', '.'},
+		{'o', 'o', '.', '.', '.', '.', '.', '.'},
 		{'.', 'o', '.', '.', '.', '.', '.', '.'},
 		{'.', '.', '.', 'o', 'o', '.', '.', 'o'}
 	};
