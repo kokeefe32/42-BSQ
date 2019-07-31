188a0d77f73c0c3a413685f8710e4f82ffe24366
Joseph Iwok <jiwok@e1z2r9p25.42.us.org>
2019-07-30 18:37:36 -0700

    main is now better

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaokeefe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jiwok <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/28 13:37:30 by kaokeefe          #+#    #+#             */
-/*   Updated: 2019/07/28 17:22:57 by jiwok            ###   ########.fr       */
+/*   Created: 2019/07/30 14:21:43 by jiwok             #+#    #+#             */
+/*   Updated: 2019/07/30 14:24:02 by jiwok            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,67 +16,104 @@
 #include <stdio.h>
 #include "funcs.h"
 
-void	create_map(char *file_name)
+void    ft_assign_elements(char *filename, char **arr, int ncols, int nrows)
 {
-	int		fd;
-	int		i;
-	int		n_columns;
-	int		n_rows;
-	char	ch;
-	char	defs[3];
-	char	buf[10];
-	
-	fd = open(file_name, O_RDONLY);
-	if (fd == -1)
-		return ;
-	i = 0;
-	n_rows = 0;
-	n_columns = 0;
-	while (read(fd, &ch, 1) > 0)
+    int     i;
+    int     j;
+    int     fd;
+    char    ch;
+    char    buf[ncols + 2];
+
+    i = 0;
+    fd = open(filename, O_RDONLY);
+    while (read(fd, &ch, 1) > 0)
 	{
-		if (ch >= '0' && ch <= '9' && !n_rows)
-			buf[i] = ch;
-		else
-		{
-			if (!n_rows)
-			{
-				n_rows = ft_atoi(buf);
-				i = 0;
-				defs[i] = ch;
-			}
-			else if (!defs[2])
-				defs[i] = ch;
-			else if (ch == '\n' && !n_columns)
-			{
-				n_columns = 1;
-				i = 0;
-			}
-			else if (ch == '\n' && n_columns)
-			{
-				n_columns = i - 1;
-				break ;
-			}
-		}
-		i += 1;
-	}
-	close(fd);
-	printf("rows %i\ncolumns %i\n", n_rows, n_columns);
-	i = 0;
-	while (i < 3)
+        if (ch == '\n')
+        {
+            while (read(fd, buf, ncols + 1) > 0) 
+            {
+                j = 0;
+                while (j < ncols)
+                {
+                    arr[i][j] = buf[j];
+                    j += 1;
+                }
+                i++;
+            }
+            break ;
+        }
+    }
+    close(fd);
+}
+
+char    **ft_demystify2(int fd, int nrows, int *ncols)
+{
+    int     i;
+    char	ch;
+    char    **arr;
+
+    arr = (char **)malloc(sizeof(char *) * nrows);
+    i = 0;
+    while (read(fd, &ch, 1) > 0)
 	{
-		printf("i: %c\n", defs[i]);
-		i++;
-	}
+        if (ch == '\n')
+            break ;
+        i += 1;
+    }
+    *ncols = i;
+    i = 0;
+    while (i < nrows)
+    {
+        arr[i] = (char *)malloc(sizeof(char) * ((*ncols) + 1));
+        i += 1;    
+    }
+
+    return (arr);
+}
+
+void    ft_demystify1(int fd, int *nrows, char *defs)
+{
+    int     i;
+    char	ch;
+    char	buf[13];
+
+    i = 0;
+    while (read(fd, &ch, 1) > 0)
+	{
+        if (ch == '\n')
+        {
+            defs[0] = buf[i - 3];
+            defs[1] = buf[i - 2];
+            defs[2] = buf[i - 1];
+            buf[i - 3] = '\0';
+            *nrows = ft_atoi(buf);
+            i = 0;
+            break ;
+        }
+        buf[i] = ch;
+        i += 1;
+    }
 }
 
 int		main(int argc, char **argv)
 {
-	int i;
+	int     i;
+	int     fd;
+    int     nrows;
+    int     ncols;
+    char    **arr;
+    char    defs[3];
 
 	i = 1;
 	while (i < argc)
 	{
-		create_map(argv[i]);
+		fd = open(argv[i], O_RDONLY); 
+        if (fd == -1)
+            continue ;
+        ft_demystify1(fd, &nrows, defs);
+        arr = ft_demystify2(fd, nrows, &ncols);
+        close(fd);
+        ft_assign_elements(argv[i], arr, ncols, nrows);
 		write(1, "\n", 1);
 		i++;
 	}
